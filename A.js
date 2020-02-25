e5217228b3b7436a170aa28bc7d708dace82dbf6
Rahul-ai <55375986+Rahul-ai@users.noreply.github.com>
2020-02-25 18:01:24 +0530

    Update A.js

--- A.js
+++ A.js
@@ -11,15 +11,16 @@ arr.splice(i,1);
 var d= dist(a.i,a.j,b.i,b.j);
 return d;
 }
-var cols=5;
-var rows=5;
-var grid=new Array(cols);
+var cols=50;  //set columns
+var rows=50;  //set rows
+var grid=new Array(cols);   
 var openSet=[];
 var closeSet=[];
 var start ;
 var end;
 var w,h;
 var path=[];
+var nosolution=false;
 function Spot(i,j){
 this.i=i;
 this.j=j;
@@ -28,12 +29,22 @@ this.g=0;
 this.h=0;
 this.neighbors=[];
 this.previous= undefined;
+this.wall=false;
+if(random(1)<0.4){
+// setting the wall
+this.wall=true;
+}
 this.show=function(color){
 fill(color);
+if(this.wall){
+fill(0);
+//giving the "Black" color to wall
+}
 noStroke(0);
 rect(this.i * w, this.j * h, w-1 , h-1);
 }
-this.addNeighbors = function(grid){
+this.addNeighbors = function(grid){ 
+// setting the neighbors for each grid
 var i=this.i;
 var j=this.j;
 if(i <cols - 1){
@@ -49,6 +60,15 @@ if(j>0)
 {
 this.neighbors.push(grid[i][j-1]);
 }  
+if(i >0&&j>0){
+this.neighbors.push(grid[i-1][j-1]);
+}
+if( i<cols-1 && j>0){
+this.neighbors.push(grid[i+1][j-1]);
+}
+if(i<cols-1 && j<rows-1){
+this.neighbors.push(grid[i+1][j+1]);
+}
 }
 }
 function setup(){
@@ -71,6 +91,8 @@ grid[i][j].addNeighbors(grid);
 }
 start=grid[0][0];
 end=grid[cols-1][rows-1];
+end.wall=false;
+start.wall=false;
 openSet.push(start);
 console.log(grid);
 }
@@ -82,6 +104,7 @@ console.log(grid);
 
 
 function draw(){
+// Doing the recursion job
 if (openSet.length>0){
 var final=0;      
 for(var i=0;i<openSet.length;i++){
@@ -90,8 +113,6 @@ for(var i=0;i<openSet.length;i++){
 }
 } 
 var current =openSet[final];
-
-
 if(current===end){	
 noLoop();
 console.log("i am Happy bro And you know why");
@@ -101,22 +122,24 @@ closeSet.push(current);
 var neighbors = current.neighbors;
 for(var i =0; i<neighbors.length;i++){
 var neighbor=neighbors[i];
-console.log("sfd");
-if(!closeSet.includes(neighbor)){
+if(!closeSet.includes(neighbor) && !neighbor.wall){
 var tempG=current.g+1; 
+  var newpath=false;
 if(openSet.includes(neighbor)){
 if(tempG < neighbor.g){
+  newpath = true;
 neighbor.g = tempG;
 } 
 }else{
 neighbor.g=tempG;
+  newpath =true;
 openSet.push(neighbor);
 }
+  if(newpath){
 neighbor.h = heuristic(neighbor,end);
-
 neighbor.f= neighbor.g+neighbor.h;
 neighbor.previous=current;
-
+  }
 } 
 }
 }
@@ -127,8 +150,11 @@ neighbor.previous=current;
 
 
 else{
-
+console.log("no Solution");
+nosolution=true;
+noloop();
 }
+
 
 background(0);
 
@@ -149,18 +175,18 @@ closeSet[i].show(color(255,0,0));
 for(var i=0;i<openSet.length;i++){
 openSet[i].show(color(0,255,0));
 }
-		path=[];
+	path=[];
 	var temp =current;
 	path.push(temp);
 while(temp.previous){
 	path.push(temp.previous);
    temp = temp.previous;
 }
+
 for(var i=0;i< path.length; i++){
 path[i].show(color(0,0,255));
 }
 }
-
 
 
 
